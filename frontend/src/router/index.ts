583ce597730ba46cda3311b1de5ab3666405a9b3
lenoxy <lenoxy@gmx.ch>
2021-04-09 14:56:21 +0200

    Replaced vue2 with vue3 with store and router

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Placeholder</div>
+</template>
+
+<script lang="ts">
+import {Vue} from 'vue-class-component';
+
+
+export default class Login extends Vue {}
+
+</script>
+
+<style scoped>
+
+</style>
