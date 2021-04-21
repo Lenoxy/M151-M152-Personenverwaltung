4e5ffdafd902f4a76ad30d87d149eddb28124223
lenoxy <lenoxy@gmx.ch>
2021-04-21 11:21:27 +0200

    Mongo connection working

--- backend/src/main/java/ch/lu/bbzw/backendpersonenverwaltung/TestController.java
+++ backend/src/main/java/ch/lu/bbzw/backendpersonenverwaltung/TestController.java
@@ -1,6 +1,8 @@
 package ch.lu.bbzw.backendpersonenverwaltung;
 
+import ch.lu.bbzw.backendpersonenverwaltung.config.MongoConfig;
 import com.mongodb.client.MongoClient;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController{
 
-    private final MongoClient mongoClient;
+    private final MongoConfig mongoConfig;
 
 
     @Autowired
-    public TestController(final MongoClient mongoClient) {
-        this.mongoClient = mongoClient;
+    public TestController(final MongoConfig mongoConfig) {
+        this.mongoConfig = mongoConfig;
     }
 
 
     @RequestMapping("/")
     public String index() {
+        Document document = new Document("type", "exam");
+        document.append("score", 300);
+        mongoConfig.mongoClient().getDatabase("person").getCollection("person").insertOne(document);
         return "Greetings from Spring Boot!";
     }
 }
--- backend/src/main/java/ch/lu/bbzw/backendpersonenverwaltung/config/MongoConfig.java
+++ backend/src/main/java/ch/lu/bbzw/backendpersonenverwaltung/config/MongoConfig.java
@@ -2,25 +2,17 @@ package ch.lu.bbzw.backendpersonenverwaltung.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig{
-    //@Value("${s3.accessKey}")
-    //private String uri = "mongodb://pers-db/person";
+    public @Bean
+    MongoClient mongoClient(){
 
-    //@Value("${s3.accessKey}")
-    //private String host = "mongoserver";
+        MongoClient mongoClient = MongoClients.create("mongodb://root:Person321@pers-db:27017");
 
-    //@Value("${s3.accessKey}")
-    //private int port = 27017;
-
-
-    public @Bean MongoClient mongoClient() {
-        return MongoClients.create("mongodb://pers-db:27017");
+        System.out.println(mongoClient.listDatabaseNames().first());
+        return mongoClient;
     }
-
-
 }
