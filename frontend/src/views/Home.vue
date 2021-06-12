71d464c907567d0bfe58db843237e2d0ce9ab301
daniel <danypolgar@gmail.com>
2021-06-12 14:32:39 +0200

    pixel graphic added

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home">
-    <h1>Fancy start page</h1>
-    <p>With uncentered logo</p>
+    <h1 class="title">Start page</h1>
     <img alt="Vue logo" src="../assets/logo.svg" class="big">
+    <h1 class="title">Roles</h1>
+    <img alt="Admin" src="../assets/admin_pixel.png" class="role">
+    <h1 class="title">Admin</h1>
+    <img alt="User" src="../assets/user_pixel.png" class="role">
+    <h1 class="title">User</h1>
   </div>
 </template>
 
@@ -20,5 +24,11 @@ export default class Home extends Vue {}
 <style scoped>
   .big{
     width: 100%;
+  }
+
+  .title {
+    width: 100%;
+    font-size: 50pt;
+    margin: auto;
   }
 </style>
